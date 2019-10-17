28b9c4d2011f721b1b8c0dbaec4f9502e1fdaf0f
Daniel Hetrick <dhetrick@gmail.com>
2019-10-17 18:25:06 -0400

    Updated license notices

--- erk/spelledit.py
+++ erk/spelledit.py
@@ -1,3 +1,33 @@
+#
+#  Erk IRC Client
+#  Copyright (C) 2019  Daniel Hetrick
+#               _   _       _                         
+#              | | (_)     | |                        
+#   _ __  _   _| |_ _  ___ | |__                      
+#  | '_ \| | | | __| |/ _ \| '_ \                     
+#  | | | | |_| | |_| | (_) | |_) |                    
+#  |_| |_|\__,_|\__| |\___/|_.__/ _                   
+#  | |     | |    _/ |           | |                  
+#  | | __ _| |__ |__/_  _ __ __ _| |_ ___  _ __ _   _ 
+#  | |/ _` | '_ \ / _ \| '__/ _` | __/ _ \| '__| | | |
+#  | | (_| | |_) | (_) | | | (_| | || (_) | |  | |_| |
+#  |_|\__,_|_.__/ \___/|_|  \__,_|\__\___/|_|   \__, |
+#                                                __/ |
+#                                               |___/ 
+#  https://github.com/nutjob-laboratories
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import re
 import fnmatch
