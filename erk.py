3a634cbe6a230ad8a32d4384fb750fee23a27619
Daniel Hetrick <dhetrick@gmail.com>
2019-10-23 02:11:34 -0400

    New commandline arguments

--- erk.py
+++ erk.py
@@ -46,34 +46,96 @@ from twisted.internet import reactor
 
 from erk import *
 from erk.common import *
+from erk.irc import connect,connectSSL,reconnect,reconnectSSL
 
 parser = argparse.ArgumentParser(
 	prog=f"python {PROGRAM_FILENAME}",
 	formatter_class=argparse.RawDescriptionHelpFormatter,
-	description=f'''
- ___      _   
+	description=f''' ___      _   
 |__ \ _ _| |__	|===============
 / _  | '_| / /	| {APPLICATION_NAME} {APPLICATION_VERSION}
 \___/|_| |_\_\\	|===============
 
 An open source IRC client
 ''',
-	epilog='''https://github.com/nutjob-laboratories/erk'''
+	epilog=f'''Official {APPLICATION_NAME} source code repository
+https://github.com/nutjob-laboratories/erk''',
+	add_help=False,
 )
 
-parser.add_argument( "-v", "--version", help=f"Display version", action="store_true")
-parser.add_argument( "-l", "--licence", help=f"Display software license", action="store_true")
+user_info = get_user()
 
-parser.add_argument( "--ontop", help=f"Display window on top of all other windows", action="store_true")
-parser.add_argument( "--fullscreen", help=f"Fill screen with application window", action="store_true")
-parser.add_argument( "--maximize", help=f"Display window maximized", action="store_true")
+proggroup = parser.add_argument_group('Optional arguments')
 
-parser.add_argument("--beat", type=int,help=f"\"Keep alive\" heartbeat interval ({str(DEFAULT_KEEPALIVE_INTERVAL)})", metavar="SECONDS")
+proggroup.add_argument("server", type=str,help="Server to connect to", metavar="SERVER")
+proggroup.add_argument("port", type=int,help="Server port to connect to (6667)", default=6667, nargs='?', metavar="PORT")
 
-parser.add_argument("--config", type=str,help="Configuration file",default=SETTINGS_FILE, metavar="FILE")
-parser.add_argument("--style", type=str,help="Text style file",default=TEXT_SETTINGS_FILE, metavar="FILE")
+netgroup = parser.add_argument_group('Server options')
+
+netgroup.add_argument( "--autojoin", help=f"Autojoin channels", action="store_true")
+netgroup.add_argument( "--ssl", help=f"Use SSL to connect to IRC", action="store_true")
+netgroup.add_argument( "--reconnect", help=f"Reconnect to servers on disconnection", action="store_true")
+
+usergroup = parser.add_argument_group('User options')
+
+usergroup.add_argument("-n","--nickname", type=str,help=f"Nickname to use ({user_info['nickname']})",default=user_info["nickname"], metavar="NICK")
+usergroup.add_argument("-a","--alternate", type=str,help=f"Alternate nickname to use ({user_info['alternate']})",default=user_info["alternate"], metavar="NICK")
+usergroup.add_argument("-u","--username", type=str,help=f"Username to use ({user_info['username']})",default=user_info["username"], metavar="USER")
+usergroup.add_argument("-r","--realname", type=str,help=f"Realname to use ({user_info['realname']})",default=user_info["realname"], metavar="NAME")
+
+infogroup = parser.add_argument_group('Application information')
+
+infogroup.add_argument( "-v", "--version", help=f"Display version", action="store_true")
+infogroup.add_argument( "-l", "--licence", help=f"Display software license", action="store_true")
+
+configgroup = parser.add_argument_group('Configuration options')
+
+configgroup.add_argument("--config", type=str,help="Load configuration file",default=SETTINGS_FILE, metavar="FILE")
+configgroup.add_argument("--style", type=str,help="Load text style file",default=TEXT_SETTINGS_FILE, metavar="FILE")
+
+optgroup = parser.add_argument_group('Options')
+
+optgroup.add_argument("-h", "--help", help=f"Displays help", action="help")
+optgroup.add_argument( "--ontop", help=f"Display window on top of all other windows", action="store_true")
+optgroup.add_argument( "--fullscreen", help=f"Fill screen with application window", action="store_true")
+optgroup.add_argument( "--maximize", help=f"Display window maximized", action="store_true")
+optgroup.add_argument("--beat", type=int,help=f"Set \"keep alive\" heartbeat interval ({str(DEFAULT_KEEPALIVE_INTERVAL)})", metavar="SECONDS")
 
 args = parser.parse_args()
+
+# ====================
+# | USER INFORMATION |
+# ====================
+
+edited_user_info = False
+new_user = {
+	"nickname": user_info['nickname'],
+	"username": user_info['username'],
+	"realname": user_info['realname'],
+	"alternate": user_info['alternate'],
+}
+
+if args.nickname!=user_info['nickname']:
+	new_user['nickname'] = args.nickname
+	edited_user_info = True
+
+if args.alternate!=user_info['alternate']:
+	new_user['alternate'] = args.alternate
+	edited_user_info = True
+
+if args.username!=user_info['username']:
+	new_user['username'] = args.username
+	edited_user_info = True
+
+if args.realname!=user_info['realname']:
+	new_user['realname'] = args.realname
+	edited_user_info = True
+
+if edited_user_info: save_user(new_user)
+
+# =======================
+# | LICENSE AND VERSION |
+# =======================
 
 if args.licence:
 	print(GPL_NOTIFICATION,end='')
@@ -109,5 +171,36 @@ if __name__ == '__main__':
 			GUI.showMaximized()
 		else:
 			GUI.show()
+
+	if args.server:
+		server = args.server
+		port = args.port
+
+		if args.autojoin:
+			GUI.autojoins[server] = loadChannels()
+
+		if args.ssl:
+			if args.reconnect:
+				func = reconnectSSL
+			else:
+				func = connectSSL
+		else:
+			if args.reconnect:
+				func = reconnect
+			else:
+				func = connect
+
+		func(
+			nickname=new_user['nickname'],
+			server=server,
+			port=port,
+			alternate=new_user['alternate'],
+			password='',
+			username=new_user['username'],
+			realname=new_user['realname'],
+			ssl=args.ssl,
+			gui=GUI,
+			reconnect=args.reconnect
+		)
 
 	reactor.run()
