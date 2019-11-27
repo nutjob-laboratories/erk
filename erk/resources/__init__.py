683f67aa72b8ed3f30c73c6a0279d121c5bde151
Daniel Hetrick <dhetrick@gmail.com>
2019-11-27 09:14:01 -0500

    Minor update
    Converted color names in text.css from HTML names to HTML hex values
    Cleaned up erk.resources/__init__.py

--- erk/resources/__init__.py
+++ erk/resources/__init__.py
@@ -1,119 +1,79 @@
 
-
 # Load in resource file
 globals()["erk.resources.resources"] = __import__("erk.resources.resources")
 
-DEFAULT_FONT = ":/LiberationMono-Regular.ttf"
+DEFAULT_FONT 			= ":/LiberationMono-Regular.ttf"
+CONNECTING_ANIMATION	= ":/connecting.gif"
+LOGO 					= ":/logo.png"
 
-ERK_ICON				= ":/gui-erk.png"
-SYSTRAY_FLASH_ICON		= ":/gui-flash.png"
-
-MDI_BACKGROUND			= ":/gui-background.png"
-USER_WINDOW_ICON		= ":/gui-user.png"
-CHANNEL_WINDOW_ICON		= ":/gui-channel.png"
-CONSOLE_WINDOW_ICON		= ":/gui-console.png"
-LOCKED_CHANNEL_ICON		= ":/gui-locked.png"
-
-USERLIST_OPERATOR_ICON	= ":/gui-ulist_op.png"
-USERLIST_VOICED_ICON	= ":/gui-ulist_voice.png"
-USERLIST_OWNER_ICON = ":/gui-ulist_owner.png"
-USERLIST_ADMIN_ICON = ":/gui-ulist_admin.png"
-USERLIST_HALFOP_ICON = ":/gui-ulist_halfop.png"
-USERLIST_NORMAL_ICON	= ":/gui-chanuser.png"
-
+ERK_ICON					= ":/gui-erk.png"
+SYSTRAY_FLASH_ICON			= ":/gui-flash.png"
+MDI_BACKGROUND				= ":/gui-background.png"
+USER_WINDOW_ICON			= ":/gui-user.png"
+CHANNEL_WINDOW_ICON			= ":/gui-channel.png"
+CONSOLE_WINDOW_ICON			= ":/gui-console.png"
+LOCKED_CHANNEL_ICON			= ":/gui-locked.png"
+USERLIST_OPERATOR_ICON		= ":/gui-ulist_op.png"
+USERLIST_VOICED_ICON		= ":/gui-ulist_voice.png"
+USERLIST_OWNER_ICON 		= ":/gui-ulist_owner.png"
+USERLIST_ADMIN_ICON 		= ":/gui-ulist_admin.png"
+USERLIST_HALFOP_ICON 		= ":/gui-ulist_halfop.png"
+USERLIST_NORMAL_ICON		= ":/gui-chanuser.png"
 HORIZONTAL_RULE_BACKGROUND	= ":/gui-horizontal_rule.png"
+ACTIVE_ICON					= ":/gui-active.png"
+CHECKED_ICON 				= ":/gui-checked.png"
+UNCHECKED_ICON 				= ":/gui-unchecked.png"
+INPUT_ICON 					= ":/gui-input.png"
+OUTPUT_ICON 				= ":/gui-output.png"
+IO_ICON 					= ":/gui-io.png"
+TEXT_WINDOW_ICON 			= ":/gui-text.png"
 
 EXIT_ICON				= ":/menu-exit.png"
 BOOKMARK_ICON			= ":/menu-bookmark.png"
-
 SERVER_ICON				= ":/menu-server.png"
 RESTART_ICON			= ":/menu-restart.png"
 FONT_ICON				= ":/menu-font.png"
 TIMESTAMP_ICON			= ":/menu-timestamp.png"
 MESSAGE_ICON			= ":/menu-message.png"
 RESIZE_ICON				= ":/menu-resize.png"
-
 AUTOCOMPLETE_ICON		= ":/menu-autocomplete.png"
 EMOJI_ICON				= ":/menu-emoji.png"
-
 MISC_ICON				= ":/menu-miscellaneous.png"
-
 CONNECTION_DISPLAY_ICON	= ":/menu-connection_display.png"
-
-SPELLCHECK_ICON				= ":/menu-spellcheck.png"
-
-ACTIVE_ICON				= ":/gui-active.png"
-
-CASCADE_ICON				= ":/menu-cascade.png"
+SPELLCHECK_ICON			= ":/menu-spellcheck.png"
+CASCADE_ICON			= ":/menu-cascade.png"
 TILE_ICON				= ":/menu-tile.png"
+USERLIST_ICON			= ":/menu-userlist.png"
+POSITION_ICON			= ":/menu-position.png"
+NETWORK_SERVER_ICON		= ":/menu-network.png"
+ABOUT_ICON 				= ":/menu-about.png"
+LINK_ICON				= ":/menu-link.png"
+LOG_ICON				= ":/menu-logs.png"
+LOG_LENGTH_ICON 		= ":/menu-loglength.png"
+KICK_ICON 				= ":/menu-kick.png"
+BAN_ICON 				= ":/menu-ban.png"
+KICKBAN_ICON 			= ":/menu-kickban.png"
+PLUS_ICON 				= ":/menu-plus.png"
+MINUS_ICON 				= ":/menu-minus.png"
+CLIPBOARD_ICON 			= ":/menu-clipboard.png"
+TOPIC_ICON 				= ":/menu-topic.png"
+NETWORKING_ICON 		= ":/menu-networking.png"
+CLEAR_ICON 				= ":/menu-clear.png"
+P_ICON 					= ":/menu-p.png"
+S_ICON 					= ":/menu-s.png"
+T_ICON 					= ":/menu-t.png"
+M_ICON 					= ":/menu-m.png"
+K_ICON 					= ":/menu-k.png"
+MODE_BAN_ICON 			= ":/menu-mode_ban.png"
+MODE_UNKNOWN_ICON 		= ":/menu-mode_unknown.png"
+ENTRY_ICON 				= ":/menu-entry.png"
+CHAT_DISPLAY_ICON 		= ":/menu-chat_display.png"
+HISTORY_ICON 			= ":/menu-history.png"
+FORMAT_ICON 			= ":/menu-format.png"
+FONT_ICON 				= ":/menu-font.png"
+USER_ICON 				= ":/menu-user.png"
+VISITED_ICON 			= ":/menu-visited.png"
+NOTIFY_ICON 			= ":/menu-notifications.png"
 
-USERLIST_ICON				= ":/menu-userlist.png"
-POSITION_ICON				= ":/menu-position.png"
-
-NETWORK_SERVER_ICON				= ":/menu-network.png"
-
-CHECKED_ICON = ":/gui-checked.png"
-UNCHECKED_ICON = ":/gui-unchecked.png"
-
-
-FANCY_NETWORK = ":/fancy-network.png"
-
-FANCY_SERVER = ":/fancy-server.png"
-
-
-
-VISITED_ICON = ":/menu-visited.png"
-
-CONNECTING_ANIMATION = ":/connecting.gif"
-
-NOTIFY_ICON = ":/menu-notifications.png"
-
-LOGO = ":/logo.png"
-ABOUT_ICON = ":/menu-about.png"
-
-LINK_ICON = ":/menu-link.png"
-LOG_ICON = ":/menu-logs.png"
-LOG_LENGTH_ICON = ":/menu-loglength.png"
-
-KICK_ICON = ":/menu-kick.png"
-BAN_ICON = ":/menu-ban.png"
-KICKBAN_ICON = ":/menu-kickban.png"
-
-PLUS_ICON = ":/menu-plus.png"
-MINUS_ICON = ":/menu-minus.png"
-
-CLIPBOARD_ICON = ":/menu-clipboard.png"
-
-TOPIC_ICON = ":/menu-topic.png"
-
-NETWORKING_ICON = ":/menu-networking.png"
-
-INPUT_ICON = ":/gui-input.png"
-OUTPUT_ICON = ":/gui-output.png"
-IO_ICON = ":/gui-io.png"
-
-CLEAR_ICON = ":/menu-clear.png"
-
-TEXT_WINDOW_ICON = ":/gui-text.png"
-
-P_ICON = ":/menu-p.png"
-S_ICON = ":/menu-s.png"
-T_ICON = ":/menu-t.png"
-M_ICON = ":/menu-m.png"
-K_ICON = ":/menu-k.png"
-MODE_BAN_ICON = ":/menu-mode_ban.png"
-MODE_UNKNOWN_ICON = ":/menu-mode_unknown.png"
-
-ENTRY_ICON = ":/menu-entry.png"
-
-CHAT_DISPLAY_ICON = ":/menu-chat_display.png"
-
-HISTORY_ICON = ":/menu-history.png"
-
-#FANCY_COLOR = ":/fancy-colors.png"
-#FANCY_FONT = ":/fancy-font.png"
-#FANCY_RESIZE = ":/fancy-resize.png"
-
-FORMAT_ICON = ":/menu-format.png"
-FONT_ICON = ":/menu-font.png"
-USER_ICON = ":/menu-user.png"
+FANCY_NETWORK	= ":/fancy-network.png"
+FANCY_SERVER 	= ":/fancy-server.png"
