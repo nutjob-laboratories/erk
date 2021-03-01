931a71ca586917cae291e3b806f00ba8a0ba7a6f
Dan Hetrick <dhetrick@gmail.com>
2021-03-01 17:13:40 -0500

    Minor update
    Changed the package file extension to ".perk" because it looks better :-)

--- erk/files.py
+++ erk/files.py
@@ -45,7 +45,7 @@ from .objects import *
 # File extensions
 SCRIPT_FILE_EXTENSION = "erk"
 STYLE_FILE_EXTENSION = "style"
-PACKAGE_FILE_EXTENSION = "erkp"
+PACKAGE_FILE_EXTENSION = "perk"
 
 # Application directories
 INSTALL_DIRECTORY = sys.path[0]
